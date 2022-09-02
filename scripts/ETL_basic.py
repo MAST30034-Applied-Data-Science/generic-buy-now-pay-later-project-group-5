e80bf20955364eb14a61e9210f089fd2f8d4408a
senturner <stturner@student.unimelb.edu.au>
2022-09-02 17:38:02 +1000

    added tag clean up to ETL script

--- scripts/ETL_basic.py
+++ scripts/ETL_basic.py
@@ -1,6 +1,7 @@
 from functools import reduce 
 from pyspark.sql import DataFrame
 from pyspark.sql import SparkSession
+from pyspark.sql import functions as f
 
 spark = (
     SparkSession.builder.appName("ETL")
@@ -20,6 +21,11 @@ transactions = spark.read.parquet('../data/tables/transactions_20210228_20210827
 cons_user_details = spark.read.parquet('../data/tables/consumer_user_details.parquet')
 tbl_merchants = spark.read.parquet('../data/tables/tbl_merchants.parquet')
 tbl_consumers = spark.read.options(delimiter='|').csv('../data/tables/tbl_consumer.csv', header = True)
+
+# Convert tags to lowercase, remove unwanted characters, and split text into array
+tbl_merchants = tbl_merchants.withColumn('tags', f.lower(f.col('tags')))
+tbl_merchants = tbl_merchants.withColumn('tags', f.regexp_replace('tags', r'[\(\)\[\]]', ''))
+tbl_merchants = tbl_merchants.withColumn('tags', f.split(f.col('tags'), ','))
 
 joined_data = transactions.join(tbl_merchants,['merchant_abn'],JOIN_TYPE)\
             .join(cons_user_details, ['user_id'],JOIN_TYPE)\
